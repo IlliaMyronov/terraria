825b228134a9f8ce8ba5a42ddee027c2e632101d
IlliaMyronov <111521194+IlliaMyronov@users.noreply.github.com>
2022-09-15 08:54:37 -0500

    one screen map

---
+++ schoolTerraria/Assets/scripts/world.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class world : MonoBehaviour
+{
+    // reference to the block systen
+    private blockSystem blockSys;
+
+    // variables for the block template
+    private GameObject blockPrefab;
+    private SpriteRenderer currentRend;
+    // 2d Array to store the world
+    int[,] world = new int[135, 240];
+
+    public void Awake()
+    {
+
+        for (int i = 0; i < 135; i++)
+        {
+
+            for (int j = 0; j < 240; j++)
+            {
+
+                if (i > 45)
+                {
+
+                    world[i, j] = 0;
+
+                }
+
+                else
+                {
+
+                    world[i, j] = -1;
+
+                }
+
+            }
+        }
+
+    }
+
+    public void drawWorld
+    {
+
+        for (int i = 0; i<world.Length; i++)
+        {
+
+            for (int j = 0; j<world[0].Length; j++)
+            {
+
+                if (world[i, j] != -1) 
+                { 
+                    
+
+
+                }
+
+            }
+
+        }
+
+    }
+}
